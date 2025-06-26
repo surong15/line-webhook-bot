d5068b566b189881ed441725965780b1cb6321a2
rong <surong015@gmail.com>
2025-06-26 14:39:56 +0800

    精簡專案，只保留 webhook 相關檔案

--- api/users.js
+++
@@ -1,6 +0,0 @@
-import { getUserIds } from './userStore.js';
-
-export default function handler(req, res) {
-  const userIds = getUserIds();
-  res.status(200).json({ count: userIds.length, userIds });
-} 
